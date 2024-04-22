1e2607525e74c7e1060ecaa639ab07a3abad3190
Alfonso Ramirez <arammes03@gmail.com>
2024-04-22 18:07:40 +0200

    Habilitando CORS

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 
+// CORS
+const cors = require('cors');
+
 // RUTAS API
 const routerApi = require('./routes/index.router');
 
@@ -16,6 +19,29 @@ const app = express();
 
 // PERMITE LECTURA Y ESCRITURA DEL JSON
 app.use(express.json());
+
+// CONFIGURACIÓN CORS
+/*
+   Definimos el origen permitido para las solicitudes
+  ! "*" es para cualquier origen (MENOS SEGURO)
+  */
+const whiteList = ['http://127.0.0.1:5500']; // Lista de dominios permitidos
+
+var corsOptions = {
+  // Vamos a comprobar que ese dominio de origen está en la lista de permitidos
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      // Si está en la lista
+      callback(null, true); // No hay error, el origen es permitido
+    } else {
+      // En caso de no estar en la lista
+      callback(new Error('Acceso no permitido')); // Ese dominio no tiene acceso y muestra un error
+    }
+  },
+};
+
+// Cargamos la configuración de CORS en la app
+app.use(cors(corsOptions));
 
 // RUTAS DE LA APP
 routerApi(app);
