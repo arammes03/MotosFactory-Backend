9ec4559423677369cd35c0ad9ab84536090a6898
Alfonso Ramirez <arammes03@gmail.com>
2024-04-22 22:09:25 +0200

    Tabla Users

--- api/MotosFactoryDB.sql
+++ api/MotosFactoryDB.sql
@@ -5,4 +5,12 @@ CREATE TABLE manufacturers (
 	location VARCHAR ( 255 ) NOT NULL,
 	year VARCHAR ( 4 ) NOT NULL
 )
+
+# # # # # # # # # # # # # USUARIOS # # # # # # # # # # # # #
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  name VARCHAR ( 255 ) NOT NULL,
+  email VARCHAR ( 255 ) UNIQUE NOT NULL,
+  password VARCHAR ( 255 ) NOT NULL
+)
 */
