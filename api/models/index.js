349558f069d9623ada0fa72d178614c89152bfe4
Alfonso Ramirez <arammes03@gmail.com>
2024-04-23 22:04:32 +0200

    ADD modelo usuario en setupModels

--- api/models/index.js
+++ api/models/index.js
@@ -1,9 +1,11 @@
 // Este fichero se encarga de mandar la conexión hacia los modelos y asi poder hacer todo el mapeo de los datos
 // Importamos los modelos
 const { Motorbike, MotorbikeSchema } = require('./motorbike.model');
+const { User, UserSchema } = require('./user.model');
 
 function setupModels(sequelize) {
   Motorbike.init(MotorbikeSchema, Motorbike.config(sequelize));
+  User.init(UserSchema, User.config(sequelize));
 }
 
 module.exports = { setupModels };
