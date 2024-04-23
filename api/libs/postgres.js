0b9e89e76d2669a7a4499f95fa2c89e3646ae841
Alfonso Ramirez <arammes03@gmail.com>
2024-04-23 16:45:22 +0200

    Configuración pool de conexiones con fichero .env

--- api/libs/postgres.js
+++ api/libs/postgres.js
@@ -4,6 +4,9 @@ const { Client } = require('pg');
 // Librería que nos permite hacer un conjunto de conexiones a la base de datos
 const { Pool } = require('pg');
 
+// Importamos la configuración de la base de datos
+const { config } = require('../config/config');
+
 // Función que hará la conexión a la base de datos
 async function getConnection() {
   // Creamos el cliente de la conexión a la base de datos
@@ -18,13 +21,14 @@ async function getConnection() {
   return client; // Devolvemos el cliente de la conexión a la base de datos para poder hacer consultas
 }
 
+// Configuramos la conexión a la base de datos
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
 // Creamos el pool de conexiones
 const pool = new Pool({
-  host: 'localhost', // Dirección del servidor
-  port: 5432, // Puerto de conexión
-  user: 'admin', // Usuario de la base de datos
-  password: 'admin123', // Contraseña del usuario de la base de datos
-  database: 'MotosFactoryDB', // Nombre de la base de datos
+  connectionString: URI,
 });
 
 // Exportamos la función de conexión a la base de datos
