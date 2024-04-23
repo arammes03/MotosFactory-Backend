21e33166a6a69bf59a14954ab42494a97882f31b
Alfonso Ramirez <arammes03@gmail.com>
2024-04-23 16:28:25 +0200

    Pool de conexiones

--- api/libs/postgres.js
+++ api/libs/postgres.js
@@ -1,6 +1,9 @@
 // Librería que nos permite conectarnos a la base de datos
 const { Client } = require('pg');
 
+// Librería que nos permite hacer un conjunto de conexiones a la base de datos
+const { Pool } = require('pg');
+
 // Función que hará la conexión a la base de datos
 async function getConnection() {
   // Creamos el cliente de la conexión a la base de datos
@@ -15,5 +18,14 @@ async function getConnection() {
   return client; // Devolvemos el cliente de la conexión a la base de datos para poder hacer consultas
 }
 
+// Creamos el pool de conexiones
+const pool = new Pool({
+  host: 'localhost', // Dirección del servidor
+  port: 5432, // Puerto de conexión
+  user: 'admin', // Usuario de la base de datos
+  password: 'admin123', // Contraseña del usuario de la base de datos
+  database: 'MotosFactoryDB', // Nombre de la base de datos
+});
+
 // Exportamos la función de conexión a la base de datos
-module.exports = getConnection;
+module.exports = { getConnection, pool };
